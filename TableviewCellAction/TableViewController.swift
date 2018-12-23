4cc2a2d6ae5718460ec3fcaeaa716608621df979
cagdaseksi <cagdas.eksi@hotmail.com>
2018-12-23 12:01:43 +0300

    initial commit.

---
+++ TableviewCellAction/TableViewController.swift
@@ -0,0 +1,85 @@
+//
+//  TableViewController.swift
+//  TableviewCellAction
+//
+//  Created by MAC on 22/12/18.
+//  Copyright © 2018 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+var seasons = ["Spring", "Summer", "Autumn", "Winter"]
+var seasonDesc = ["All the threes and plants are in green", "It is very hot in the summer.", "Leaves fall off threes.", "The weather is very cold in the winter."]
+var myIndex = 0
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return seasons.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+         cell.textLabel?.text = seasons[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        myIndex = indexPath.row
+        performSegue(withIdentifier: "segue", sender: self)
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TableviewCellAction/ViewController.swift
+++ TableviewCellAction/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var descLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        titleLabel.text = seasons[myIndex]
+        descLabel.text = seasonDesc[myIndex]
+        myImageView.image = UIImage(named: seasons[myIndex] + ".jpg")
+        setBackgroundColor(season: seasons[myIndex])
     }
 
+    func setBackgroundColor(season:String) {
+        
+        switch season {
+        case "Spring":
+            self.view.backgroundColor = UIColor.green
+        case "Summer":
+            self.view.backgroundColor = UIColor.yellow
+        case "Autumn":
+            self.view.backgroundColor = UIColor.brown
+        case "Winter":
+            self.view.backgroundColor = UIColor.white
+        default:
+            self.view.backgroundColor = UIColor.white
+        }
+        
+    }
 
 }
 
